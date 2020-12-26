e0c3caa83b746b547b1c40399ae4a7ba4d4e3cb3
Syamnath <72413647+ahamdesigner@users.noreply.github.com>
2020-12-26 20:55:24 +0530

    Update index.php
    ചിരി

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 // Silence is golden.
 ?>
+class സരസ്വതി extends HTMLElement {
+  constructor() {
+    വിദ്യ(); // always call വിദ്യ() first in the constructor.
+  }
+  connectedCallback() {
+    // Silence is golden.
+  }
+  disconnectedCallback() {
+    // Silence is golden.
+  }
+  attributeChangedCallback(attrName, Val, updateVal) {
+    // Smile is golden.
+  }
+}
