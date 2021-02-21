f61cc64f23328520fead2241d40f70847882a99b
Syamnath <syamnathsnair33@gmail.com>
2021-02-21 18:40:27 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * Update/Install.
+ * Update/Install moved to perfume
  *
  */
-
-require path . 'beauty.php';
-wp_enqueue_script( 'hello.php' );
-if ( action( ['ദുർഗ്ഗ'] ) {
-	വിദ്യ( പൂജ['ദേവി']);
-}
 ?>
